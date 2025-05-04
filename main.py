fd7f0aea1c2bb0f1bbea869ed049279fa0b52aac
elonprogdev <mordoki2021@gmail.com>
2025-05-04 17:30:07 +0300

    Activate 'input_data' function instead 'input_name'

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def index():
-    if request.args.get('name'):
-        return render_template("index.html", user_name = request.args.get('name'))
+    if request.args.get('name') and request.args.get('age'):
+        return render_template("index.html", user_name = request.args.get('name'), user_age = "("+ request.args.get('age') + ")")
     else:
         return render_template("index.html")
         
@@ -17,10 +17,11 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route ("/input_name", methods=["GET"])
-def input_name():
+@app.route ("/input_data", methods=["GET"])
+def input_data():
     name = request.args["name"]
-    return redirect(url_for('index', name=name))
+    age = request.args["age"]
+    return redirect(url_for('index', name=name, age=age))
 
 
 
