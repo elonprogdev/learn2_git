d8036595b8dff1f8a7263caeefaae500b6912bf2
elonprogdev <mordoki2021@gmail.com>
2025-05-04 16:38:31 +0300

    Clean files fm SocketIO

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect
-from flask_socketio import SocketIO
 import requests
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "secret key"
-socket_app = SocketIO(app, async_mode="eventlet")
 
 app = Flask(__name__)
 
@@ -19,17 +14,6 @@ def index():
 def about():
     return render_template("about.html")
 
-@socket_app.on("guess")
-def handle_guess(name):
-  
-    user = name
-    print('gurs notify')
-    
-    try:
-        socket_app.emit("response",  f"Привет {user}!", to=request.sid)
-        
-    except ValueError:
-        socket_app.emit("response", "Invalid input. Enter a number.", to=request.sid)
 
 if __name__ == "__main__":
     app.run(debug=True)
