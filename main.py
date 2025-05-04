43a3bcff9e6e097f4603aae51e77b29a70fa3e49
elonprogdev <mordoki2021@gmail.com>
2025-05-04 17:06:24 +0300

    Active 'input_name' function

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template, request, url_for, redirect
-import requests
+
 
 app = Flask(__name__)
 
-@app.route("/")
+
+
+@app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
-
-
-
+    if request.args.get('name'):
+        return render_template("index.html", user_name = request.args.get('name'))
+    else:
+        return render_template("index.html")
+        
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route ("/input_name", methods=["GET"])
+def input_name():
+    name = request.args["name"]
+    return redirect(url_for('index', name=name))
+
 
 
 if __name__ == "__main__":
